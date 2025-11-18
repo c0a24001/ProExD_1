b7c1750564aa77c9a220e3a1529e98b0c2beaf18
c0a24001 <c0a2400130@edu.teu.ac.jp>
2025-11-18 17:51:49 +0900

    演習

--- pygame_image.py
+++ pygame_image.py
@@ -11,7 +11,8 @@ def main():
     clock  = pg.time.Clock()
     bg_img = pg.image.load("fig/pg_bg.jpg")
     bg_img2 = pg.transform.flip(bg_img,True,False)
-    kk_img = pg.transform.flip(pg.image.load("fig/3.png"),True,False)
+    kk_img = pg.image.load("fig/3.png") #練習3:こうかとん画像の読み込み
+    kk_img = pg.transform.flip(kk_img, True, False) #練習3反転
     kk_rct = kk_img.get_rect()
     kk_rct.center = 300,200
 
@@ -23,20 +24,20 @@ def main():
         # print(key_lst[pg.K_UP])
         move_vector = [-1, 0]
 
-        if key_lst[pg.K_UP]: # 上矢印キー
-           move_vector[1] -= 1 # 上に移動
-        if key_lst[pg.K_DOWN]: # 下矢印キー
-           move_vector[1] += 1 # 下に移動
-        if key_lst[pg.K_LEFT]: # 左矢印キー
-           move_vector[0] -= 1 # 左に移動
-        if key_lst[pg.K_RIGHT]: # 右矢印キー
-           move_vector[0] += 2 # 右に移動
+        if key_lst[pg.K_UP]: 
+           move_vector[1] -= 1 
+        if key_lst[pg.K_DOWN]: 
+           move_vector[1] += 1 
+        if key_lst[pg.K_LEFT]: 
+           move_vector[0] -= 1 
+        if key_lst[pg.K_RIGHT]: 
+           move_vector[0] += 2 
         kk_rct.move_ip(move_vector)
 
         x = tmr %800
         screen.blit(bg_img, [-x, 0])
         screen.blit(bg_img, [-x+800, 0])
-        screen.blit(kk_img, [300, 200])
+        screen.blit(bg_img, [-x+3200, 0])
         screen.blit(kk_img,kk_rct)
         pg.display.update()
         tmr += 1        
